28cef0102edb40354cdfd16328bb16729d21d878
Salvador-97 <salvador.go_97@hotmail.com>
2025-10-21 21:41:02 -0600

    fix-iconos-lenguajes

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,74 +1,77 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay, Navigation } from "swiper/modules";
 
-import {
-    estado, lenguajes, proyectoContenedores, proyectoMarbetes, proyectoSeries, proyectoPortafolio, lenguajesContenedores, lenguajesMarbetes, lenguajesSeries, lenguajesPortafolio, iconos
-}
-    from "../utils/informacionProyectos";
+import { proyectos, listaProyectos, estado, lenguajes } from "../utils/informacionProyectos";
 import clsx from 'clsx';
 import { useState } from 'react';
 
-export function Tarjeta({ informacion, iconos, lenguajesProyecto }) {
+export function Tarjeta({ informacion, lenguajesProyecto }) {
+    console.log("Lenguajes: ", lenguajesProyecto);
     const [masInfo, verInformacion] = useState(false);
     return (
         <>
             <div className={clsx(
                 "rounded-[1.5rem] mx-auto w-[100%] pb-[1rem] flex flex-col"
             )}>
-                <div className='text-center rounded-t-[1.5rem] w-full ' style={{ background: iconos.colorFondo }}>
-                    <i className={`${iconos.icono} fa-6x py-[0.8rem]`} style={{ color: iconos.colorIcono }}></i>
+                <div className='text-center rounded-t-[1.5rem] w-full bg-moradotarjeta text-blanco'>
+                    <i className={`${informacion.icono} fa-6x py-[0.8rem]`}></i>
                 </div>
                 <div className='relative flex justify-center'>
                     <div className={
-                    clsx(
-                        "text-[#fff] text-[0.9rem] text-center m-[1rem] flex flex-col items-center justify-center",
-                        "transition-all duration-800 absolute",
-                        masInfo ? 'translate-y-[160%]' : 'opacity-100'
-                    )}>
-                    <div className="font-[700] h-[3.5rem] pb-[1rem]">{informacion.nombre}</div>
-                    <div className="font-[700]">Tecnologias</div>
-                    <div className="py-[0.5rem]">
-                        {lenguajesProyecto.map((lenguaje, index) => (
-                            <i key={index} className={`${lenguajes[lenguaje].icono} pr-[0.5rem] text-[1.2rem]`}
-                                style={{ color: lenguajes[lenguaje].colorIcono }}></i>
-                            // <img src={lenguajes[lenguaje].img} className="w-[10%] mx-auto" />
-                        ))}
-                    </div>
-                    <div className="pb-[1rem]">
-                        <i className="fa-solid fa-gear fa-spin px-[0.5rem]"></i>
-                        {estado(informacion.desarrollo, 'Completo', 'En desarrollo')}
-                        <i className="fa-solid fa-gear fa-spin px-[0.5rem]"></i>
-                    </div>
-                    <div>
-                        <i className='fa-solid fa-circle mr-[0.5rem]'
-                            style={{ color: estado(informacion.estado, '#63E6BE', '#E84646') }}>
-                        </i>
-                        {estado(informacion.estado, 'Online', 'Offline')}
-                    </div>
-                </div>
-                <div className={clsx(
-                    "flex flex-col justify-center items-center text-center text-[#fff] m-[0.5rem]",
-                    "transition-all duration-800 w-[90%]",
-                    masInfo ? 'opacity-100' : 'opacity-0'
-                )}>
-                    <div className="mb-[1rem] h-[6rem]">
-                        {informacion.descripcion}
-                    </div>
-                    <div className="flex flex-col w-[80%]">
+                        clsx(
+                            "text-[#fff] text-[0.9rem] text-center m-[1rem] flex flex-col items-center justify-center",
+                            "transition-all duration-800 absolute",
+                            masInfo ? 'translate-y-[160%]' : 'opacity-100'
+                        )}>
+                        <div className="font-[700] h-[3.5rem] pb-[1rem]">{informacion.nombre}</div>
+                        <div className="font-[700]">Tecnologias</div>
+                        <div className="py-[0.5rem] flex justify-center">
+                            {lenguajesProyecto.map((lenguaje, index) => (
+                                !lenguajes[lenguaje].img
+                                ? <i key={index} className={`${lenguajes[lenguaje].icono} pr-[0.5rem] text-[1.2rem]`}
+                                  style={{ color: lenguajes[lenguaje].colorIcono }}></i>
+                                : <img alt='icono' src={lenguajes[lenguaje].img} className="w-[13%] pr-[0.5rem]" />
+                            ))}
+                        </div>
+                        <div className="pb-[1rem]">
+                            <i className="fa-solid fa-gear fa-spin px-[0.5rem]"></i>
+                            {estado(informacion.desarrollo, 'Completo', 'En desarrollo')}
+                            <i className="fa-solid fa-gear fa-spin px-[0.5rem]"></i>
+                        </div>
                         <div>
-                            Progreso
+                            <i className='fa-solid fa-circle mr-[0.5rem]'
+                                style={{ color: estado(informacion.estado, '#63E6BE', '#E84646') }}>
+                            </i>
+                            {estado(informacion.estado, 'Online', 'Offline')}
                         </div>
-                        <div className="h-[0.5rem] bg-[#8A5CF6] rounded-full overflow-hidden my-[0.5rem]"
-                            style={{ width: `${informacion.progreso * 100}%` }}>
-                        </div>
-                        <p className="text-xs text-[#ccc] mt-1">
-                            {Math.round(informacion.progreso * 100)}%
-                        </p>
                     </div>
-                    <a href={informacion.link} className='w-full my-[0.5rem] font-[700]'>Visitar sitio 
-                        <i className="fa-solid fa-up-right-from-square text-[0.8rem] text-blanco pl-[0.5rem]"></i>
-                    </a>
-                </div>
+                    <div className={clsx(
+                        "flex flex-col justify-center items-center text-center text-[#fff] m-[0.5rem]",
+                        "transition-all duration-800 w-[90%]",
+                        masInfo ? 'opacity-100' : 'opacity-0'
+                    )}>
+                        <div className="mb-[1rem] h-[6rem]">
+                            {informacion.descripcion}
+                        </div>
+                        <div className="flex flex-col w-[80%]">
+                            <div>
+                                Progreso
+                            </div>
+                            <div className="h-[0.5rem] bg-[#8A5CF6] rounded-full overflow-hidden my-[0.5rem]"
+                                style={{ width: `${informacion.progreso * 100}%` }}>
+                            </div>
+                            <p className="text-xs text-[#ccc] mt-1">
+                                {Math.round(informacion.progreso * 100)}%
+                            </p>
+                        </div>
+                        <a href={informacion.link} className={clsx(
+                            'w-full my-[0.5rem] font-[700]',
+                            masInfo ? 'pointer-events-auto' : 'pointer-events-none'
+                            )}>
+                        Visitar sitio
+                            <i className="fa-solid fa-up-right-from-square text-[0.8rem] text-blanco pl-[0.5rem]"></i>
+                        </a>
+                    </div>
                 </div>
                 <button type='button' className='cursor-pointer w-full text-blanco z-20' onClick={() => verInformacion(!masInfo)}>
                     <i className={clsx(
@@ -84,24 +87,29 @@ export function Tarjeta({ informacion, iconos, lenguajesProyecto }) {
 export function SlidesProyectos() {
     return (
         <>
-        <Swiper
-            modules={[Pagination, Autoplay, Navigation]}
-            navigation={true}
-            pagination={{el: '.custom-pagination' , clickable: true }}
-            spaceBetween={20}
-            slidesPerView={1}
-            loop={true}
-            speed={800}
-            breakpoints={{
-                480: { slidesPerView: 2 },
-                640: { slidesPerView: 2 },   // móviles
-                768: { slidesPerView: 2 },   // tablets
-                1024: { slidesPerView: 3 },  // pantallas medianas
-                1280: { slidesPerView: 4 }   // pantallas grandes
-            }}
-            className='rounded-b-[1rem] m-auto h-[25rem]'
-        >
-            <SwiperSlide className='bg-azulfondo rounded-[1.5rem]'>
+            <Swiper
+                modules={[Pagination, Autoplay, Navigation]}
+                navigation={true}
+                pagination={{ el: '.custom-pagination', clickable: true }}
+                spaceBetween={20}
+                slidesPerView={1}
+                loop={true}
+                speed={800}
+                breakpoints={{
+                    480: { slidesPerView: 2 },
+                    640: { slidesPerView: 2 },   // móviles
+                    768: { slidesPerView: 2 },   // tablets
+                    1024: { slidesPerView: 3 },  // pantallas medianas
+                    1280: { slidesPerView: 4 }   // pantallas grandes
+                }}
+                className='rounded-b-[1rem] m-auto h-[25rem]'
+            >
+                {proyectos.map(proyecto => (
+                    <SwiperSlide key={proyecto} className='bg-azulfondo rounded-[1.5rem]'>
+                        <Tarjeta  informacion={listaProyectos[proyecto]} lenguajesProyecto={listaProyectos[proyecto].lenguajes} />
+                    </SwiperSlide>
+                ))}
+                {/* <SwiperSlide className='bg-azulfondo rounded-[1.5rem]'>
                 <Tarjeta informacion={proyectoPortafolio} iconos={iconos.portafolio} lenguajesProyecto={lenguajesPortafolio} />
             </SwiperSlide>
             <SwiperSlide className='bg-azulfondo rounded-[1.5rem]'>
@@ -113,8 +121,11 @@ export function SlidesProyectos() {
             <SwiperSlide className='bg-azulfondo rounded-[1.5rem]'>
                 <Tarjeta informacion={proyectoSeries} iconos={iconos.series} lenguajesProyecto={lenguajesSeries} />
             </SwiperSlide>
-        </Swiper>
-        <div className="custom-pagination mt-4 text-center"></div>
+            <SwiperSlide className='bg-azulfondo rounded-[1.5rem]'>
+                <Tarjeta informacion={proyectoHospital} iconos={iconos.hospital} lenguajesProyecto={lenguajesHospital} />
+            </SwiperSlide> */}
+            </Swiper>
+            <div className="custom-pagination mt-4 text-center"></div>
         </>
     );
 }
--- src/utils/informacionProyectos.js
+++ src/utils/informacionProyectos.js
@@ -1,27 +1,3 @@
-
-export const iconos = {
-    contenedores: {
-        icono: 'fa-solid fa-truck-ramp-box',
-        colorIcono: '#F5F5F4',
-        colorFondo: '#6D44E0'
-    },
-    series: {
-        icono: 'fa-solid fa-circle-play',
-        colorIcono: '#F5F5F4',
-        colorFondo: '#6D44E0'
-    },
-    marbetes: {
-        icono: 'fa-solid fa-file-circle-check',
-        colorIcono: '#F5F5F4',
-        colorFondo: '#6D44E0'
-    },
-    portafolio: {
-        icono: 'fa-solid fa-address-card',
-        colorIcono: '#F5F5F4',
-        colorFondo: '#6D44E0'
-    }
-}
-
 export const lenguajes = {
     html: {
         icono: 'fa-brands fa-html5',
@@ -48,75 +24,124 @@ export const lenguajes = {
         colorIcono: '#3776AB',
         img: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg'
     },
-    cvs: {
-        icono: 'fa-brands fa-css3',
-        colorIcono: '#0074D9',
-        img: ''
-    },
     excel: {
         icono: 'fa-solid fa-file-excel',
         colorIcono: '#217346',
-        img: ''
+        img: false
     },
     sql: {
         icono: 'fa-solid fa-database',
         colorIcono: '#9FA6A2',
-        img: ''
+        img: false
     },
     node: {
         icono: 'fa-brands fa-node-js',
         colorIcono: '#339933',
-        img: ''
+        img: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original.svg'
     },
     react: {
         icono: 'fa-brands fa-react',
         colorIcono: '#61DAFB',
-        img: ''
+        img: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg'
+    },
+    csharp: {
+        icono: 'devicon-csharp-plain',
+        colorIcono: '',
+        img: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg'
+    },
+    tailwind: {
+        icono: '',
+        colorIcono: '',
+        img: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg'
+    },
+    flask: {
+        icono: 'devicon-flask-original',
+        colorIcono: '',
+        img: false
+    },
+    sqlserver: {
+        icono: 'devicon-microsoftsqlserver-plain',
+        colorIcono: '',
+        img: false
+    },
+    dotnet: {
+        icono: 'devicon-dotnetcore-plain',
+        colorIcono: '',
+        img: false
     }
 }
 
-export const proyectoContenedores = {
+export const contenedores = {
+    icono: 'fa-solid fa-truck-ramp-box',
     nombre: 'Sistema de control y registro de contenedores',
     descripcion: 'Aplicación web para gestionar contenedores y productos en un almacén, con seguimiento de tarimas y ubicación.',
-    link: '#',
+    link: 'https://github.com/Salvador-97/Inbound_Manager-Web',
     desarrollo: false,
     estado: false,
-    progreso: 1
+    progreso: 1,
+    lenguajes: ['bootstrap', 'javascript', 'python', 'flask', 'sql']
 }
 
-export const proyectoMarbetes = {
+export const marbetes = {
+    icono: 'fa-solid fa-file-circle-check',
     nombre: 'Sistema de generación y gestión de marbetes',
     descripcion: 'Aplicación de escritorio para crear y administrar marbetes de productos, con manejo automatizado de datos.',
-    link: '#',
+    link: 'https://github.com/Salvador-97/Inbound_Manager',
     desarrollo: false,
     estado: true,
-    progreso: 0.75
+    progreso: 0.75,
+    lenguajes: ['python', 'excel']
 }
 
-export const proyectoSeries = {
+export const series = {
+    icono: 'fa-solid fa-circle-play',
     nombre: 'Plataforma de series web',
     descripcion: 'Sitio web para explorar y ver series, con información detallada de temporadas y capítulos.',
-    link: '#',
+    link: 'https://github.com/Salvador-97/Rari-Caturas-TV/tree/fix-css',
     desarrollo: true,
     estado: false,
-    progreso: 0.8
+    progreso: 0.8,
+    lenguajes: ['html', 'css', 'javascript', 'sql', 'node']
 }
 
-export const proyectoPortafolio = {
+export const portafolio = {
+    icono: 'fa-solid fa-address-card',
     nombre: 'Portafolio Web',
     descripcion: 'Sitio web diseñado para presentar mis proyectos, habilidades y experiencia laboral.',
     link: 'https://salvador-97.github.io/Portafolio/',
     desarrollo: true,
     estado: true,
-    progreso: 1
+    progreso: 1,
+    lenguajes: ['javascript', 'react', 'tailwind']
 }
+
+export const hospital = {
+    icono: 'fa-solid fa-stethoscope',
+    nombre: 'Sistema de simulacion de un hospital',
+    descripcion: 'Gestión de pacientes de un hospital así como de trabajadores.',
+    link: '#',
+    desarrollo: false,
+    estado: false,
+    progreso: 0.1,
+    lenguajes: ['sqlserver', 'csharp', 'dotnet']
+}
+
+export const listaProyectos = {
+    portafolio: portafolio,
+    contenedores: contenedores,
+    series: series,
+    marbetes: marbetes,
+    hospital: hospital
+}
+
+export const proyectos = ['portafolio', 'contenedores', 'series', 'marbetes', 'hospital']
 
 export const lenguajesContenedores = ['html', 'css', 'bootstrap', 'javascript', 'python', 'sql'];
 export const lenguajesMarbetes = ['python', 'cvs', 'excel'];
 export const lenguajesSeries = ['html', 'css', 'javascript', 'sql', 'node'];
 export const lenguajesPortafolio = ['react', 'javascript', 'css']
 
-export function estado (estado, verdadero, falso){
+export function estado(estado, verdadero, falso) {
     let valorEstado = ''
     if (estado) {
         valorEstado = verdadero;
