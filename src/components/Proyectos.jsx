6581f686967ce5552980f82c65df320b548ab8c0
Salvador-97 <salvador.go_97@hotmail.com>
2026-02-15 00:05:50 -0600

    fix(Enlace clickeable de toda la tarjeta)

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -4,14 +4,14 @@ import clsx from 'clsx';
 export function Tarjeta({ informacion }) {
     console.log("Proyecto: ", informacion)
     return (
-        <>
+        <div className="relative">
             <div className={clsx(
                 "rounded-[1.5rem] border border-blanco",
                 "flex flex-col group",
                 "bg-white/5 hover:bg-white/10 backdrop-blur-md border border-white/10 rounded-2xl",
                 "shadow-lg transition-all duration-300 hover:shadow-xl hover:-translate-y-1"
             )}>
-                <a href="#" className='flex mx-[1.5rem] mt-[1rem]'>
+                <div className='flex mx-[1.5rem] mt-[1rem]'>
                     <div className={clsx(
                         'border-2 border-white/10 transition-all duration-300 group-hover:border-white/40 w-16 h-16 rounded-full flex justify-center items-center text-center'
                     )}>
@@ -25,7 +25,7 @@ export function Tarjeta({ informacion }) {
                         <span className='text-gray-100'>2 semanas</span>
                     </div>
 
-                </a>
+                </div>
                 <div className='m-[1.5rem]'>
                     <h1 className='text-blanco text-[1.2rem] font-bold '>Inbound Manager Web</h1>
                     <div className='flex mt-[0.5rem]'>
@@ -44,10 +44,10 @@ export function Tarjeta({ informacion }) {
                     <div className='text-blanco w-[45%]'>
                         <span>Progreso</span>
                         <div className="h-[0.8rem] bg-gradienteBarra rounded-full overflow-hidden my-[0.5rem]"
-                                style={{ width: `${informacion.progreso * 100}%` }}>
+                            style={{ width: `${informacion.progreso * 100}%` }}>
                         </div>
                         <span className="text-xs text-[#ccc] mt-1">
-                                {Math.round(informacion.progreso * 100)}%
+                            {Math.round(informacion.progreso * 100)}%
                         </span>
                         <div>
                             <i className='fa-solid fa-circle mr-[0.5rem]'
@@ -57,15 +57,15 @@ export function Tarjeta({ informacion }) {
                         </div>
                     </div>
                     <div className='w-[50%] flex flex-col items-center justify-between'>
-                        <a href="#" 
-                        className={clsx(
-                            'w-full rounded-[1rem] border border-rosa transition-all duration-300 hover:bg-rosa  text-blanco text-center p-[0.4rem] font-bold'
-                        )}>
+                        <a href="/Proyecto"
+                            className={clsx(
+                                'w-full rounded-[1rem] border border-rosa transition-all duration-300 hover:bg-rosa  text-blanco text-center p-[0.4rem] font-bold z-10'
+                            )}>
                             Demo
                             <i className="fa-solid fa-arrow-up-right-from-square ml-[0.5rem]"></i>
                         </a>
                         <a href="#" className={clsx(
-                            'w-full flex items-center justify-around rounded-[1rem] border border-rosa transition-all duration-300  hover:bg-rosa text-blanco text-center p-[0.4rem] font-bold'
+                            'w-full flex items-center justify-around rounded-[1rem] border border-rosa transition-all duration-300  hover:bg-rosa text-blanco text-center p-[0.4rem] font-bold z-10'
                         )}>
                             <i className="fa-brands fa-github"></i>
                             GitHub
@@ -73,6 +73,8 @@ export function Tarjeta({ informacion }) {
                         </a>
                     </div>
                 </div>
+                <a href="#" className="absolute inset-0"></a>
+
                 {/* <div className='relative flex justify-center'>
                     <div className={
                         clsx(
@@ -89,7 +91,7 @@ export function Tarjeta({ informacion }) {
                                         style={{ color: lenguajes[lenguaje].colorIcono }}></i>
                                     : <img alt='icono' src={lenguajes[lenguaje].img} className="w-[13%] pr-[0.5rem]" />*/}
             </div>
-        </>
+        </div>
     );
 }
 
