e0eb0b0bef5e27cb906f2d36ddcd411f17faf103
Salvador-97 <salvador.go_97@hotmail.com>
2026-03-10 22:02:54 -0600

    fix(Efecto hover boton)

--- src/components/AcercaMi.jsx
+++ src/components/AcercaMi.jsx
@@ -66,9 +66,9 @@ export default function AcercaMi() {
                 <h2 className='titulo text-rosaPalido font-[800] text-[1.2rem]'>Full-Stack Developer</h2>
                 <p className='text-blanco text-center mt-[0.5rem] max-w-[40ch]'>{aboutMe}</p>
                 <a href='/Portafolio/CV_Dev_Salvador_Gutiérrez_Olvera.pdf' download className={clsx(
-                    'max-w-[60%] mt-4 border-rosaBoton border-2 text-rosaBoton p-2 rounded-[0.5rem]',
+                    'max-w-[60%] mt-4 bg-rosaBoton text-blanco border-rosaBoton border-2 p-2 rounded-[0.5rem]',
                     'flex justify-around transition-colors ease-in 300ms',
-                    'hover:bg-rosaBoton hover:text-blanco',
+                    'lg:bg-transparent lg:text-rosaBoton lg:hover:bg-rosaBoton hover:text-blanco',
                 )}>
                     <ArrowDownToLine />Descargar CV
                 </a>
